584f68df7365dadb969ac30664c027f9cfe2c7c6
K <fieldprogrammable@gmail.com>
2019-01-12 02:37:34 +0900

    BufferedWaveProviderがうまく使えない

--- asio4split/asio4split/Program.cs
+++ asio4split/asio4split/Program.cs
@@ -13,26 +13,80 @@ namespace asio4split {
         [STAThread]
         static void Main(string[] args) {
             var sampleRate = 48000;
-            var caps = AsioOut.GetDriverNames();
+            var bitDepth = 24;
+            var byteDepth = bitDepth / 8;
+            var devices = AsioOut.GetDriverNames();
+            // 使用するデバイスを決定する
             Console.WriteLine("# Input Device");
-            var srcIndex = SelectDeviceIndex(caps);
+            var srcIndex = SelectDeviceIndex(devices);
             Console.WriteLine("# Output Device");
-            var dstIndex = SelectDeviceIndex(caps);
+            var dstIndex = SelectDeviceIndex(devices);
+            // デバイスのルーティング設定
+            // TEST: srcのMASTER OUT L, RをdstのLRに当てることにする
+            var routingChannels = new int[] { 12, 13 }; // srcのチャンネル番号を並べて書く
 
-            using (var src = new AsioOut(caps[srcIndex]))
-            using (var dst = new AsioOut(caps[dstIndex])) {
-                float[] buffer = null;
+
+            // あとはよしなに
+            using (var src = new AsioOut(devices[srcIndex]))
+            using (var dst = new AsioOut(devices[dstIndex])) {
+                // ルーティングテーブルがイマイチの場合に補完する
+                var routingChannelDiff = dst.DriverOutputChannelCount - routingChannels.Length;
+                if (routingChannelDiff > 0) {
+                    Console.WriteLine($"Warning: RoutingChannel added {routingChannelDiff} empty channles");
+                    routingChannels = routingChannels.Concat(Enumerable.Repeat(-1, routingChannelDiff)).ToArray();
+                } else if (routingChannelDiff < 0) {
+                    Console.WriteLine($"Warning: RoutingChannel resize {routingChannelDiff}");
+                    routingChannels = routingChannels.Take(dst.DriverOutputChannelCount).ToArray();
+                }
+                Console.WriteLine($"RoutingChannels: [{string.Join(",", routingChannels)}]");
+                // Output Setting
+                var bufferProvider = new BufferedWaveProvider(new WaveFormat(sampleRate, bitDepth, dst.DriverOutputChannelCount));
+                dst.Init(bufferProvider);
+                dst.Play();
+                // Input Setting
+                float[] srcBuffer = null;
+                byte[] dstBuffer = null;
                 src.AudioAvailable += (sx, ex) => {
-                    if (buffer == null) {
-                        buffer = new float[ex.SamplesPerBuffer * src.DriverInputChannelCount];
+                    if (srcBuffer == null) {
+                        srcBuffer = new float[ex.SamplesPerBuffer * src.DriverInputChannelCount]; // 1ch[0:511], 2ch[512:... nch
+                        dstBuffer = new byte[ex.SamplesPerBuffer * dst.DriverOutputChannelCount * byteDepth];
 
-                        Console.WriteLine($"DriverName: {src.DriverName}");
-                        Console.WriteLine($"DriverInputChannelCount: {src.DriverInputChannelCount}");
-                        Console.WriteLine($"SamplePerBuffer: {ex.SamplesPerBuffer}");
-                        Console.WriteLine($"AsioSampleType: {ex.AsioSampleType}");
-                        Console.WriteLine($"bufferSize: {buffer.Length}");
+                        Console.WriteLine("# Source");
+                        Console.WriteLine($"\tDriverName: {src.DriverName}");
+                        Console.WriteLine($"\tDriverInputChannelCount: {src.DriverInputChannelCount}");
+                        Console.WriteLine($"\tDriverOutputChannelCount: {src.DriverOutputChannelCount}");
+                        Console.WriteLine($"\tSamplePerBuffer: {ex.SamplesPerBuffer}");
+                        Console.WriteLine($"\tAsioSampleType: {ex.AsioSampleType}");
+                        Console.WriteLine($"\tsrcBufferSize: {srcBuffer.Length}");
+
+                        Console.WriteLine("# Destination");
+                        Console.WriteLine($"\tDriverName: {dst.DriverName}");
+                        Console.WriteLine($"\tDriverInputChannelCount: {dst.DriverInputChannelCount}");
+                        Console.WriteLine($"\tDriverOutputChannelCount: {dst.DriverOutputChannelCount}");
+                        Console.WriteLine($"\tdstBufferSize: {dstBuffer.Length}");
                     }
-                    ex.GetAsInterleavedSamples(buffer);
+                    ex.GetAsInterleavedSamples(srcBuffer);
+
+                    // データの書き込み src -> dst
+                    for (int dataPtr = 0; dataPtr < ex.SamplesPerBuffer; ++dataPtr) {
+                        for (int ch = 0; ch < routingChannels.Length; ++ch) {
+                            // src 1sampleのデータをbyteDepthに分割して書き込む
+                            UInt32 raw;
+                            if (routingChannels[ch] == -1) {
+                                raw = 0;
+                            } else {
+                                var srcPtr = dataPtr + (routingChannels[ch] * ex.SamplesPerBuffer);
+                                Int32 sample = (Int32)(srcBuffer[srcPtr] * Int32.MaxValue);
+                                raw = (UInt32)sample;
+                            }
+
+                            var dstPtr = ((dataPtr * routingChannels.Length) + ch) * byteDepth;
+                            for (int byteOffset = 0; byteOffset < byteDepth; ++byteOffset) {
+                                dstBuffer[dstPtr + byteOffset] = (byte)((raw >> (8 * (3 - byteOffset))) & 0xff);
+                            }
+                        }
+                    }
+                    bufferProvider.AddSamples(dstBuffer, 0, dstBuffer.Length);
                 };
                 src.InitRecordAndPlayback(null, src.DriverInputChannelCount, sampleRate);
                 src.Play();
