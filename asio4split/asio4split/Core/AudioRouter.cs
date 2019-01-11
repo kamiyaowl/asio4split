502c87b1a6fe079e1e468a776e13ee5a5c1cb423
K <fieldprogrammable@gmail.com>
2019-01-11 22:44:27 +0900

    デバイス取得を実装

---
+++ asio4split/asio4split/Core/AudioRouter.cs
@@ -0,0 +1,25 @@
+﻿using NAudio.CoreAudioApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asio4split.Core {
+    class AudioRouter {
+        /// <summary>
+        /// ルーティング元のデバイス
+        /// </summary>
+        public MMDevice CaptureDevice { get; internal set; }
+        /// <summary>
+        /// ルーティング先のデバイス
+        /// </summary>
+        public MMDevice RenderDevice { get; internal set; }
+        /// <summary>
+        /// 利用可能なデバイスを取得します
+        /// </summary>
+        /// <returns></returns>
+        public static MMDeviceCollection GetEndPoints(DataFlow dataFlow, DeviceState deviceState = DeviceState.Active) =>
+            (new MMDeviceEnumerator()).EnumerateAudioEndPoints(dataFlow, deviceState);
+    }
+}
--- asio4split/asio4split/Program.cs
+++ asio4split/asio4split/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using asio4split.Core;
+using NAudio.CoreAudioApi;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,6 +9,33 @@ using System.Threading.Tasks;
 namespace asio4split {
     class Program {
         static void Main(string[] args) {
+            var caps = AudioRouter.GetEndPoints(DataFlow.Capture).ToArray();
+            var renders = AudioRouter.GetEndPoints(DataFlow.Render).ToArray();
+
+            // TODO: UUIDみたいな固有値でStore/Restoreできるように
+            Console.WriteLine("# Capture Device");
+            var cap = SelectDevice(caps);
+            Console.WriteLine("# Render Device");
+            var render = SelectDevice(renders);
+
+            Console.ReadKey();
+
+        }
+        /// <summary>
+        /// ユーザーに使用するデバイスを選択させます
+        /// </summary>
+        /// <param name="devices"></param>
+        /// <returns></returns>
+        private static MMDevice SelectDevice(MMDevice[] devices) {
+            foreach (var x in devices.Select((x, i) => new { Index = i, Value = x })) {
+                Console.WriteLine($"[{x.Index}] {x.Value}");
+            }
+            Console.Write($"\r\ndevice index[0 - {devices.Length - 1}] >");
+            if (!int.TryParse(Console.ReadLine(), out int index) || index < 0 || index >= devices.Length) {
+                Console.WriteLine("Invalid index");
+                Environment.Exit(1);
+            }
+            return devices[index];
         }
     }
 }
